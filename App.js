0a90ae58b6517dc4a6da1ab709552fba6990e671
Sao I Kuan <saoikuan@gmail.com>
2022-09-20 18:56:14 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Cadence (Sao I) Kuan</Text>
+      <Text>301204757</Text>
       <StatusBar style="auto" />
     </View>
   );
